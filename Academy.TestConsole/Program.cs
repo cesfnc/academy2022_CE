06dd137e2bb25805130cb63c64a03a591eb0906c
Cesare Ellero (EURIS) <cesare.ellero@euris.it>
2022-04-19 18:03:36 +0200

    primo progetto

---
+++ Academy.TestConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Academy.TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int, double, decimal, float
+            //string
+            //bool
+            //enum
+
+
+            string myString3 = null;
+            string nullString = null;
+            string myString2 = "test ";
+            myString3 = "stringa 3";
+
+            var result = myString2 + myString3;
+            result = string.Concat("ss", "www");
+
+            bool isNull = string.IsNullOrEmpty(nullString);
+
+            var boolToString = isNull.ToString();
+
+            //bool hasC = myString3.Contains(nullString);
+            int indexA = myString3.IndexOf("a");
+
+            Console.WriteLine("nullString is null? " + isNull);
+            Console.WriteLine(result);
+            Console.WriteLine("a is on position " + indexA);
+
+            myString3 = "";
+
+            int a = 12;
+            int b = 8;
+            var c = (a + b) * a;
+            var d = c / a;
+            var e = Math.Pow(a, 2);
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Academy.TestConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Academt.TestConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Academt.TestConsole")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4187b99-6f64-4bbc-b0ab-d90f8433fcfe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
