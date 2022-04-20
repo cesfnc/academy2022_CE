62b5b4424c7bf15092c400d65f287156b6fdca48
Cesare Ellero (EURIS) <cesare.ellero@euris.it>
2022-04-20 10:45:14 +0200

    aggiunto string[]

---
+++ Academy.TestConsole/Constants/ConsoleConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy.TestConsole.Constants
+{
+    static public class ConsoleConstants
+    {
+        public const string ConsoleName = "Test Console";
+    }
+}
--- Academy.TestConsole/Program.cs
+++ Academy.TestConsole/Program.cs
@@ -1,27 +1,52 @@
 ﻿using System;
+using Academy.TestConsole.Constants;
 
 namespace Academy.TestConsole
 {
     internal class Program
     {
+        public const string ConsoleName = "Test Console";
+        public const int IntConstant1 = 12;
+        const bool BoolConstant1 = false;
+
         static void Main(string[] args)
         {
+            string[] test0;
+            string[] test1 = new string[0];
+
+            //Console.WriteLine(args[0]);
+
+            string concatArgs2 = string.Join(", ", args);
+            Console.WriteLine(concatArgs2);
+
             //int, double, decimal, float
             //string
             //bool
             //enum
 
+            var MyStringList2 = new string[] { "wer" , "ert"};
+            var MyStringList3 = new string[3] { "wer", "ert", "asd" };
+
+            int length = MyStringList3.Length; //3
+            string pos0 = MyStringList3[0]; //"wer"
+            string pos1 = MyStringList3[1]; //"ert"
+            string pos2 = MyStringList3[2]; //"asd"
+
+            MyStringList3.SetValue("ertertert", 0);
+            //MyStringList3.SetValue("ertertert", 5); non si può fare l'indice (5) è maggiore della capienza (3) della array
 
             string myString3 = null;
             string nullString = null;
-            string myString2 = "test ";
+            string myString2 = ConsoleName;
             myString3 = "stringa 3";
 
             var result = myString2 + myString3;
             result = string.Concat("ss", "www");
 
+            //string.IsNullOrEmpty() metodo associato al tipo
             bool isNull = string.IsNullOrEmpty(nullString);
 
+            // isNull.ToString() metodo associato all'istanza
             var boolToString = isNull.ToString();
 
             //bool hasC = myString3.Contains(nullString);
@@ -37,7 +62,7 @@ namespace Academy.TestConsole
             int b = 8;
             var c = (a + b) * a;
             var d = c / a;
-            var e = Math.Pow(a, 2);
+            double e = Math.Pow(a, 2);
             
             
 
